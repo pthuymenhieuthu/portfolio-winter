8a89b38c122b9af3ac981472ef49befe27be116a
Thuy <phuongthuy101222@gmail.com>
2024-10-15 20:15:32 +0700

    3 thành 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,7 +147,7 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                I chose to highlight three key projects:
+                I chose to highlight four key projects:
                 </p>
               </div>
             </div>
