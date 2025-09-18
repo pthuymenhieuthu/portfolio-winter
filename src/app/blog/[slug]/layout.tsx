6f1a818fa0101cbe202338c75cb52179ba4940bd
Thuy <phuongthuy101222@gmail.com>
2025-09-18 19:54:17 +0700

    thêm layout mới cho blog huhu

---
+++ src/app/blog/[slug]/layout.tsx
@@ -0,0 +1,44 @@
+import Navbar from "@/components/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { DATA } from "@/data/resume";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "../globals.css"; // 👈 nhớ đổi path vì đang ở folder blog
+import { ScrollProgress } from "@/components/ui/scroll-progress";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Blog | " + DATA.name,
+  description: DATA.description,
+};
+
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased max-w-5xl mx-auto py-12 sm:py-24 px-6", // 👈 đổi 2xl → 5xl
+          fontSans.variable
+        )}
+      >
+        <ScrollProgress />
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <TooltipProvider delayDuration={0}>
+            {children}
+            <Navbar />
+          </TooltipProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
