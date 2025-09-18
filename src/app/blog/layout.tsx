38ffcbe8c0678a2d9e72e923a6224768df141dd2
Thuy <phuongthuy101222@gmail.com>
2025-09-18 20:06:00 +0700

    thử import luôn thằng layout nhưng chỉ chỉnh width thui

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,15 +1,5 @@
-import "../globals.css";
-import Navbar from "@/components/navbar";
-import { ThemeProvider } from "@/components/theme-provider";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { ScrollProgress } from "@/components/ui/scroll-progress";
-import { Inter as FontSans } from "next/font/google";
+import RootLayout from "../layout"; // 👈 import layout gốc
 import { cn } from "@/lib/utils";
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export default function BlogLayout({
   children,
@@ -17,23 +7,14 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
+    <RootLayout>
+      <div
         className={cn(
-          "min-h-screen bg-background font-sans antialiased max-w-5xl mx-auto py-12 sm:py-24 px-6",
-          fontSans.variable
+          "min-h-screen bg-background font-sans antialiased max-w-5xl mx-auto py-12 sm:py-24 px-6"
         )}
       >
-        {/* 👇 thanh progress bar */}
-        <ScrollProgress />
-
-        <ThemeProvider attribute="class" defaultTheme="light">
-          <TooltipProvider delayDuration={0}>
-            {children}
-            <Navbar /> {/* 👈 vẫn có Navbar để back home */}
-          </TooltipProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+        {children}
+      </div>
+    </RootLayout>
   );
 }
