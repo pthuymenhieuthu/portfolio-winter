6330de9d7429931640df8fc70596e686423e3dcc
Thuy <phuongthuy101222@gmail.com>
2025-09-18 11:35:24 +0700

    thử đổi cái div chỗ projects giống với mấy cái khác coi nó có fill ra không

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -8,7 +8,7 @@ export const DATA = {
   location: "Ho Chi Minh City, Vietnam",
   locationLink: "https://www.google.com/maps/place/hochiminh",
   description:
-    "Proactive Product Designer · Passionate about learning and growth",
+    "Proactive UI/UX Designer · Passionate about learning and growth",
   summary:
     "My name is **Thuy (Winter)**. I recently graduated in July and have accumulated valuable experience in the UI/UX field. During my internship at **FireGroup**, I gained foundational knowledge of how product designers collaborate with various stakeholders within a sprint team model. Additionally, my experience working at **TikTok Shop** involved frequent use of Figma and provided me with a strong understanding of business requirements, further enhancing my practical skills and insights into the industry. Find out more about my journey and projects [below](/#work).",
   avatarUrl: "/me.jpg",
