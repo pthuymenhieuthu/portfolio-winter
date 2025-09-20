f25eb601d0f43ae9c032dfb01c65cd1d21e706a6
Thuy <phuongthuy101222@gmail.com>
2025-09-20 13:02:55 +0700

    cho con nextjs hiểu là t đang chèn vô youtube embed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import createMDX from '@next/mdx';
+
 /** @type {import('next').NextConfig} */
+const withMDX = createMDX({
+  extension: /\.mdx?$/, // cho phép import .md và .mdx
+});
+
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'], // hỗ trợ file md/mdx như page
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
